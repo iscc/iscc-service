3a0e814808c3a44107c1d04cd8366ef722db0310
Titusz Pan <titusz.pan@gmail.com>
2020-02-01 00:02:36 +0100

    - Handle file processing without temporary copy (except for videos) - Allow metadata with file upload. - Move docs to root url

--- iscc_service/main.py
+++ iscc_service/main.py
@@ -1,21 +1,32 @@
 import os
+import shutil
 import uuid
 from os.path import join, splitext
-
+from typing import Optional, List
 import uvicorn
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 import iscc
-from starlette.responses import RedirectResponse
+from tika import detector, parser
+from iscc_cli.const import SUPPORTED_MIME_TYPES, GMT
 import iscc_service
 from iscc_service.tools import code_to_bits, code_to_int
 from pydantic import BaseModel, Field, HttpUrl
-from iscc_cli.lib import iscc_from_file, iscc_from_url
-from iscc_cli.utils import iscc_split
-from iscc_cli import APP_DIR
+from iscc_cli.lib import iscc_from_url
+from iscc_cli.utils import iscc_split, get_title, mime_to_gmt
+from iscc_cli import APP_DIR, audio_id, video_id
 from starlette.middleware.cors import CORSMiddleware
+from starlette.status import (
+    HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+    HTTP_422_UNPROCESSABLE_ENTITY,
+)
 
 
-app = FastAPI(title="ISCC Web Service API", version=iscc_service.__version__,)
+app = FastAPI(
+    title="ISCC Web Service API",
+    version=iscc_service.__version__,
+    description="Microservice for creating ISCC Codes from Media Files.",
+    docs_url="/",
+)
 
 ALLOWED_ORIGINS = os.environ.get("ALLOWED_ORIGINS", "*").split()
 
@@ -28,10 +39,14 @@ app.add_middleware(
 )
 
 
-class Meta(BaseModel):
-    title: str = Field("", description="The title of an intangible creation.")
-    extra: str = Field(
-        "",
+class Metadata(BaseModel):
+    """Metadata for Meta-ID creation."""
+
+    title: str = Field(
+        ..., description="The title of an intangible creation.", min_length=1
+    )
+    extra: Optional[str] = Field(
+        None,
         description="An optional short statement that distinguishes "
         "this intangible creation from another one for the "
         "purpose of forced Meta-ID uniqueness.",
@@ -43,69 +58,141 @@ class Text(BaseModel):
 
 
 class ISCC(BaseModel):
-    iscc: str = Field(None, description="Full ISCC Code")
-    norm_title: str = Field(None, description="Normalized Title")
-    tophash: str = Field(None, description="Normalized Title")
-    gmt: str = Field(None, description="Generic Media Type")
-    bits: list = Field(None, description="Per component bitstrings")
+    """Full ISCC Code including Metadata."""
+
+    iscc: str = Field(..., description="Full ISCC Code")
+    title: str = Field(None, description="Title of intangible creation")
+    title_trimmed: str = Field(None, description="Normalized and trimmed title")
+    extra: str = Field(None, description="Optional extra metadata")
+    extra_trimmed: str = Field(
+        None, description="Normalized and trimmed extra metadata"
+    )
+    tophash: str = Field(..., description="Normalized Title")
+    gmt: str = Field(..., description="Generic Media Type")
+    bits: List[str] = Field(..., description="Per component bitstrings")
 
 
 class IsccComponent(BaseModel):
-    code: str = Field(None, description="Single ISCC component", max_length=13)
-    bits: str = Field(None, description="Bitstring of component body", max_length=64)
+    """A single ISCC Component as code, bits and headerless integer."""
+
+    code: str = Field(..., description="Single ISCC component", max_length=13)
+    bits: str = Field(..., description="Bitstring of component body", max_length=64)
     ident: int = Field(
-        None, description="Integer representation of component body", le=2 ** 64
+        ..., description="Integer representation of component body", le=2 ** 64
     )
 
 
 class MetaID(IsccComponent):
-    title: str
-    title_trimmed: str
-    extra: str
-    extra_trimmed: str
+    """A Meta-ID ISCC Component including Metadata."""
+
+    title: str = Field(..., description="Title of intangible creation")
+    title_trimmed: str = Field(..., description="Normalized and trimmed title")
+    extra: str = Field(None, description="Optional extra metadata")
+    extra_trimmed: str = Field(
+        None, description="Normalized and trimmed extra metadata"
+    )
 
 
 class ContentID(IsccComponent):
+    """A Content-ID ISCC Component including Generic Media Type."""
+
     gmt: str = Field(
         "text", description="Generic Media Type of Content-ID", max_length=64
     )
 
 
 class DataID(IsccComponent):
+    """A Data-ID ISCC Component."""
+
     pass
 
 
 class InstanceID(IsccComponent):
+    """An Instance-ID ISCC Component including Tophash."""
+
     tophash: str = Field(
         None, description="Hex-encoded 256-bit Top Hash", max_length=64
     )
 
 
-@app.get("/", summary="Redirect to API Docs")
-def root():
-    """Redirects to API Documentation"""
-    response = RedirectResponse(url="/docs")
-    return response
-
-
 @app.post(
     "/generate/from_file",
     response_model=ISCC,
+    response_model_exclude_unset=True,
     tags=["generate"],
-    summary="Generate ISCC from file",
+    summary="Generate ISCC from File",
 )
-def from_file(file: UploadFile = File(...)):
-    """Generate Full ISCC from Media File."""
-    _, ext = splitext(file.filename)
-    fn = "{}{}".format(uuid.uuid4(), ext)
-    tmp_path = join(APP_DIR, fn)
-    with open(tmp_path, "wb") as outf:
-        outf.write(file.file.read())
-    r = iscc_from_file(tmp_path, guess=False)
-    os.remove(tmp_path)
-    components = iscc_split(r["iscc"])
-    r["bits"] = [code_to_bits(c) for c in components]
-    return r
+def from_file(
+    file: UploadFile = File(...), title: str = Form(""), extra: str = Form("")
+):
+    """Generate Full ISCC Code from Media File with optional explicit metadata."""
+
+    media_type = detector.from_buffer(file.file)
+    if media_type not in SUPPORTED_MIME_TYPES:
+        raise HTTPException(
+            HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            "Unsupported media type '{}'. Please request support at "
+            "https://github.com/iscc/iscc-service/issues.".format(media_type),
+        )
+
+    file.file.seek(0)
+    tika_result = parser.from_buffer(file.file)
+
+    if not title:
+        title = get_title(tika_result, guess=True)
+
+    mid, norm_title, norm_extra = iscc.meta_id(title, extra)
+    gmt = mime_to_gmt(media_type)
+    if gmt == GMT.IMAGE:
+        file.file.seek(0)
+        cid = iscc.content_id_image(file.file)
+    elif gmt == GMT.TEXT:
+        text = tika_result["content"]
+        if not text:
+            raise HTTPException(HTTP_422_UNPROCESSABLE_ENTITY, "Could not extract text")
+        cid = iscc.content_id_text(tika_result["content"])
+    elif gmt == GMT.AUDIO:
+        file.file.seek(0)
+        features = audio_id.get_chroma_vector(file.file)
+        cid = audio_id.content_id_audio(features)
+    elif gmt == GMT.VIDEO:
+        file.file.seek(0)
+        _, ext = splitext(file.filename)
+        fn = "{}{}".format(uuid.uuid4(), ext)
+        tmp_path = join(APP_DIR, fn)
+        with open(tmp_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        features = video_id.get_frame_vectors(tmp_path)
+        cid = video_id.content_id_video(features)
+        os.remove(tmp_path)
+
+    file.file.seek(0)
+    did = iscc.data_id(file.file)
+    file.file.seek(0)
+    iid, tophash = iscc.instance_id(file.file)
+
+    if not norm_title:
+        iscc_code = "-".join((cid, did, iid))
+    else:
+        iscc_code = "-".join((mid, cid, did, iid))
+
+    components = iscc_split(iscc_code)
+
+    result = dict(
+        iscc=iscc_code,
+        tophash=tophash,
+        gmt=gmt,
+        bits=[code_to_bits(c) for c in components],
+    )
+    if norm_title:
+        result["title"] = title
+        result["title_trimmed"] = norm_title
+    if norm_extra:
+        result["extra"] = extra
+        result["extra_trimmed"] = norm_extra
+
+    file.file.close()
+    return result
 
 
 @app.post(
@@ -125,21 +212,27 @@ def from_url(url: HttpUrl):
 @app.post(
     "/generate/meta_id/",
     response_model=MetaID,
+    response_model_exclude_unset=True,
     tags=["generate"],
     summary="Generate ISCC Meta-ID",
 )
-def meta_id(meta: Meta):
-    """Generate MetaID from 'title' and 'extra'"""
-    mid, title_trimmed, extra_trimmed = iscc.meta_id(meta.title, meta.extra)
-    return {
+def meta_id(meta: Metadata):
+    """Generate MetaID from 'title' and optional 'extra' metadata"""
+    extra = meta.extra or ""
+    mid, title_trimmed, extra_trimmed = iscc.meta_id(meta.title, extra)
+    result = {
         "code": mid,
         "bits": code_to_bits(mid),
         "ident": code_to_int(mid),
         "title": meta.title,
         "title_trimmed": title_trimmed,
-        "extra": meta.extra,
-        "extra_trimmed": extra_trimmed,
     }
+
+    if extra:
+        result["extra"] = extra
+        result["extra_trimmed"] = extra_trimmed
+
+    return result
 
 
 @app.post(
@@ -193,7 +286,7 @@ def instance_id(file: UploadFile = File(...)):
     tags=["generate"],
     summary="Generate ISCC Data-ID and Instance-ID",
 )
-def data_and_instance_id(file: UploadFile = File(...)):
+def data_and_instance_id(file: UploadFile = File(...,)):
     """Generate Data-ID and Instance-ID from raw binary data"""
 
     did = iscc.data_id(file.file)
