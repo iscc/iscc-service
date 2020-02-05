cd1b0725b7e0b9b6dd4d6deadf07aa913403bced
Titusz Pan <titusz.pan@gmail.com>
2020-02-05 21:01:50 +0100

    Refactor models into their own module.

--- iscc_service/main.py
+++ iscc_service/main.py
@@ -2,15 +2,23 @@ import os
 import shutil
 import uuid
 from os.path import join, splitext
-from typing import Optional, List
 import uvicorn
 from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 import iscc
 from tika import detector, parser
 from iscc_cli.const import SUPPORTED_MIME_TYPES, GMT
 import iscc_service
+from iscc_service.models import (
+    Metadata,
+    Text,
+    ISCC,
+    MetaID,
+    ContentID,
+    DataID,
+    InstanceID,
+)
 from iscc_service.tools import code_to_bits, code_to_int
-from pydantic import BaseModel, Field, HttpUrl
+from pydantic import HttpUrl
 from iscc_cli.lib import iscc_from_url
 from iscc_cli.utils import iscc_split, get_title, mime_to_gmt
 from iscc_cli import APP_DIR, audio_id, video_id
@@ -37,82 +45,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-class Metadata(BaseModel):
-    """Metadata for Meta-ID creation."""
-
-    title: str = Field(
-        ..., description="The title of an intangible creation.", min_length=1
-    )
-    extra: Optional[str] = Field(
-        None,
-        description="An optional short statement that distinguishes "
-        "this intangible creation from another one for the "
-        "purpose of forced Meta-ID uniqueness.",
-    )
-
-
-class Text(BaseModel):
-    text: str = Field(None, description="Extracted full plain text for Content-ID.")
-
-
-class ISCC(BaseModel):
-    """Full ISCC Code including Metadata."""
-
-    iscc: str = Field(..., description="Full ISCC Code")
-    title: str = Field(None, description="Title of intangible creation")
-    title_trimmed: str = Field(None, description="Normalized and trimmed title")
-    extra: str = Field(None, description="Optional extra metadata")
-    extra_trimmed: str = Field(
-        None, description="Normalized and trimmed extra metadata"
-    )
-    tophash: str = Field(..., description="Normalized Title")
-    gmt: str = Field(..., description="Generic Media Type")
-    bits: List[str] = Field(..., description="Per component bitstrings")
-
-
-class IsccComponent(BaseModel):
-    """A single ISCC Component as code, bits and headerless integer."""
-
-    code: str = Field(..., description="Single ISCC component", max_length=13)
-    bits: str = Field(..., description="Bitstring of component body", max_length=64)
-    ident: int = Field(
-        ..., description="Integer representation of component body", le=2 ** 64
-    )
-
-
-class MetaID(IsccComponent):
-    """A Meta-ID ISCC Component including Metadata."""
-
-    title: str = Field(..., description="Title of intangible creation")
-    title_trimmed: str = Field(..., description="Normalized and trimmed title")
-    extra: str = Field(None, description="Optional extra metadata")
-    extra_trimmed: str = Field(
-        None, description="Normalized and trimmed extra metadata"
-    )
-
-
-class ContentID(IsccComponent):
-    """A Content-ID ISCC Component including Generic Media Type."""
-
-    gmt: str = Field(
-        "text", description="Generic Media Type of Content-ID", max_length=64
-    )
-
-
-class DataID(IsccComponent):
-    """A Data-ID ISCC Component."""
-
-    pass
-
-
-class InstanceID(IsccComponent):
-    """An Instance-ID ISCC Component including Tophash."""
-
-    tophash: str = Field(
-        None, description="Hex-encoded 256-bit Top Hash", max_length=64
-    )
 
 
 @app.post(
